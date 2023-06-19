308e527565a9c1b9a9f61b410c0ec5081cb0352b
JordiManyer <j.manyer@hotmail.com>
2023-06-19 11:29:16 +1000

    minor changes

--- profile_cuda_ncu.sh
+++ profile_cuda_ncu.sh
@@ -14,7 +14,10 @@ module load nvidia-hpc-sdk/22.11
 export LD_LIBRARY_PATH=~/bin/julia-1.8.5/lib/julia/:$LD_LIBRARY_PATH
 
 ncu --set full -k regex:gpu_mul --target-processes all -o result_v1 julia --project=. test/GPU_v1.jl
+ncu --set full -k regex:gpu_mul --target-processes all -o result_v2 julia --project=. test/GPU_v2.jl
 ncu --set full -k regex:gpu_mul --target-processes all -o result_v3 julia --project=. test/GPU_v3.jl
+ncu --set full -k regex:gpu_mul --target-processes all -o result_v5 julia --project=. test/GPU_v5.jl
+ncu --set full -k regex:gpu_mul --target-processes all -o result_v7 julia --project=. test/GPU_v7.jl
 
 #ncu launch --trace cuda julia --project=. -e'include("test/GPU_v0.jl")'
 #ncu launch --trace cuda julia --project=. -e'include("test/GPU_v1.jl")'
